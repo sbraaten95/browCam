36bd00b86de735556b19c8f4af65a1a360b2bbdf
Scott Braaten <sbraaten95@gmail.com>
2017-07-08 15:20:53 -0400

    now frame shows webcam and does not stop when dragged

--- content.js
+++ content.js
@@ -1,13 +1,36 @@
-var div = document.createElement("div");
+var div = document.createElement("video");
 div.id = "browCam";
-div.style.width = "50px";
-div.style.height = "100px";
+div.style.width = "150px";
+div.style.height = "300px";
 div.style.background = "black";
 div.style.position = "absolute";
 div.style.top = "50px";
 div.style.left = "50px";
-$(function() {
-	$('#browCam').draggable();
+
+$(document).ready(function() {
+	$('#browCam').draggable({
+		stop: function(event, ui) {
+			navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+			if (navigator.getUserMedia) {
+				navigator.getUserMedia({video: true}, streamWebCam, throwError);
+			}
+		}
+	});
 });
 
+function streamWebCam(stream) {
+	div.src = window.URL.createObjectURL(stream);
+	div.play();
+}
+
+function throwError(error) {
+	alert(error.name);
+}
+
 document.children[0].appendChild(div);
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+if (navigator.getUserMedia) {
+	navigator.getUserMedia({video: true}, streamWebCam, throwError);
+}
