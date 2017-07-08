88e9daaa45abecaef4fc9f169470e0b78aa37592
Scott Braaten <sbraaten95@gmail.com>
2017-07-08 13:46:20 -0400

    successfully interacts chrome DOM

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
 var div = document.createElement("div");
 div.style.width = "50px";
 div.style.height = "100px";
-div.style.color = "black";
+div.style.background = "black";
 div.style.position = "absolute";
 div.style.top = "50px";
 div.style.left = "50px";
 
-div.innerHTML = "Hello";
-
-document.appendChild(div);
+document.children[0].appendChild(div);
