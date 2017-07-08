4cdc054a55e2cfd7502fedb0d6eb46bc77a257ed
Scott Braaten <sbraaten95@gmail.com>
2017-07-08 13:44:34 -0400

    trying to get a frame to pop up. need to work with chrome DOM

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-alert('test');
+var div = document.createElement("div");
+div.style.width = "50px";
+div.style.height = "100px";
+div.style.color = "black";
+div.style.position = "absolute";
+div.style.top = "50px";
+div.style.left = "50px";
+
+div.innerHTML = "Hello";
+
+document.appendChild(div);
