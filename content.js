05c271c9afca4145c514adc8da1564f37853de6d
Scott Braaten <sbraaten95@gmail.com>
2017-07-08 14:20:33 -0400

    frame is draggable via jquery

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
 var div = document.createElement("div");
+div.id = "browCam";
 div.style.width = "50px";
 div.style.height = "100px";
 div.style.background = "black";
 div.style.position = "absolute";
 div.style.top = "50px";
 div.style.left = "50px";
+$(function() {
+	$('#browCam').draggable();
+});
 
 document.children[0].appendChild(div);
